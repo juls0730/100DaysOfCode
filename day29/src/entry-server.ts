11912b17dd26d79c2478ec7dc51df25791fcae12
juls0730 <juliangarner240@gmail.com>
2022-10-21 23:14:34 -0500

    add day 29

---
+++ day29/src/entry-server.ts
@@ -0,0 +1,15 @@
+import { compileToString } from './lib/templateRenderer';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { appState } from './main';
+let ctx = {};
+export function SSRPage(template: string) {
+	return compileToString(template);
+}
+
+export function setContext(context: Record<string, unknown>) {
+	ctx = context;
+}
+
+export function getContext() {
+	return ctx;
+}
---
+++ day29/src/main.ts
@@ -0,0 +1,21 @@
+import { Reactive } from './lib/ReactiveObject';
+import { getCookie } from './lib/cookieManager';
+
+export const appState: Reactive = new Reactive({
+	count: 0,
+	text: '',
+	cookie: getCookie('username'),
+	html: '',
+	year: '',
+	cookiedata: '',
+});
+
+let SSR: boolean;
+
+export function isSSR() {
+	if (!import.meta.env.SSR) {
+		if (SSR !== undefined) return SSR;
+		SSR = !!document.getElementById('app')?.getAttribute('data-server-rendered');
+		return SSR;
+	}
+}
