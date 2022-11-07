f56e01f9da87fdbde6e69695b974f84d1a3f8694
juls0730 <juliangarner240@gmail.com>
2022-11-06 19:48:11 -0600

    add day 45

---
+++ day45/src/entry-client.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { appState, initAppState } from './main'; 
+
+
+async function initClient() {
+	if (import.meta.env.SSR) return;
+	await initAppState();
+	await import('./style.css');
+	const { renderPage } = await import('./lib/router/pageRenderer');
+	await renderPage();
+
+	window.onpopstate = async (e: PopStateEvent) => {
+		if (e.state === null) {
+			return;
+		}
+		await renderPage();
+	};
+}
+
+await initClient();
