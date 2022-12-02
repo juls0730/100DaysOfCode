2a335746800c6eb992b4b5ab83780bf96ccf4192
juls0730 <juliangarner240@gmail.com>
2022-12-01 22:40:02 -0600

    add day 70

---
+++ day70/src/entry-client.ts
@@ -0,0 +1,18 @@
+import { initAppState } from './main'; 
+
+async function initClient() {
+	if (import.meta.env.SSR) return;
+	await initAppState();
+	await import('./style.css');
+	const { renderPage } = await import('./lib/router/pageRenderer');
+	await renderPage();
+
+	window.onpopstate = async (e: PopStateEvent) => {
+		if (e.state === null) {
+			return;
+		}
+		await renderPage();
+	};
+}
+
+await initClient();
---
+++ day70/src/lib/lruCache.ts
@@ -0,0 +1,35 @@
+// ruthlessly stolen from @trunarla on twitter
+export class LRUCache {
+	#cache: Map<string, string>;
+	#capacity: number;
+
+	constructor(capacity: number) {
+		this.#capacity = capacity;
+		this.#cache = new Map<string, string>();
+	}
+
+	set(key: string, value: string) {
+		// If we're at capacity, we need to delete the least-recently-used item:
+		if (this.#cache.size >= this.#capacity) {
+			// Manually invoke the keys iterator to get the least-recently-used key:
+			const keyToDelete = this.#cache.keys().next().value;
+			this.#cache.delete(keyToDelete);
+		}
+		this.#cache.delete(key);
+		this.#cache.set(key, value);
+	}
+
+	get(key: string) {
+		if (this.#cache.has(key)) {
+			const value = this.#cache.get(key);
+			if (!value) return;
+			this.#cache.delete(key);
+			this.#cache.set(key, value);
+			return value;
+		}
+	}
+
+	getCache() {
+		return this.#cache;
+	}
+}
