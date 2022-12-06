228edcdb03a1b30041d5c1bf53c1552e63bc8c07
juls0730 <juliangarner240@gmail.com>
2022-12-05 22:00:22 -0600

    add day 74

---
+++ day74/src/lib/router/hydrationManager.ts
@@ -0,0 +1,269 @@
+import { getAppState } from '../../main';
+import { renderPage } from './pageRenderer';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { getCookie, setCookie } from '../cookieManager';
+import { Reactive } from '../ReactiveObject';
+
+// function to turn the template into reactive content "hydating" a page
+export async function hydratePage(reduceJavascript = false) {
+	if (import.meta.env.SSR) return;
+
+	const appState = await getAppState();
+	const documentBody = document.getElementById('app');
+
+	if (!documentBody) {
+		throw new Error('Fatal Error: element with id app not found');
+	}
+
+	const hydrateEvent = (eventName, elementSelector) => {
+		hydrateElement(elementSelector, appState, eventName, getCookie, setCookie);
+	};
+
+	ReactifyTemplate(appState);
+
+	hydrateHeadElements(appState);
+	hydrateIfAttributes(appState);
+	await hydrateAnchorElements(reduceJavascript);
+
+	// interactive hydration
+	hydrateEvent('click', '*[d-on:click]');
+	hydrateEvent('pointerenter', '*[d-on:pointerEnter]');
+	hydrateEvent('pointerleave', '*[d-on:pointerExit]');
+	hydrateEvent('mousedown', '*[d-on:mouseDown]');
+	hydrateEvent('mouseup', '*[d-on:mouseUp]');
+
+	// Update app state items with input values from elements with the "d-model" attribute.
+	// Similar to Vue.js' v-model attribute.
+	hydrateModelAttributes(appState);
+	hydrateKeyDown(appState);
+}
+
+export function ReactifyTemplate(appState: Reactive) {
+	const spanElements = Array.from(document.querySelectorAll('span'));
+
+	spanElements.forEach(spanElement => {
+		// Get all attributes that start with "data-token-"
+		const reactiveElms = Array.from(spanElement.attributes).filter(attr => attr.name.startsWith('data-token-'));
+
+		if (reactiveElms.length === 0) return;
+
+		reactiveElms.forEach(reactiveElm => {
+			const item = reactiveElm.name;
+			const uuid = item.split('data-token-')[1];
+
+			// If the uuid is not found, throw an error
+			if (!uuid) throw new Error('Internal error: decoded uuid not found');
+
+			// Decode the uuid
+			let decodedUuid = '';
+			for (let i = 0; i < uuid.length; i += 2) {
+				decodedUuid += String.fromCharCode(parseInt(uuid.substr(i, 2), 16));
+			}
+
+			// If the span element's parent has the "d-once" attribute, remove it and return since we're only doing it once
+			if (spanElement.parentElement?.hasAttribute('d-once')) {
+				spanElement.parentElement.removeAttribute('d-once');
+				return;
+			}
+
+			// If the span element's parent has the "d-html" attribute, listen to the decoded uuid
+			// and set the span element's innerHTML to the change value
+			if (spanElement.parentElement?.hasAttribute('d-html')) {
+				appState.listen(decodedUuid, change => spanElement.innerHTML = change);
+				return;
+			}
+
+			appState.listen(decodedUuid, change => spanElement.textContent = change);
+		});
+	});
+}
+
+export function hydrateIfAttributes(appState: Reactive) {
+	const conditionalElms = Array.from(document.querySelectorAll('*[d-if]'));
+	if (conditionalElms.length === 0) return;
+	conditionalElms.forEach(async (e: Element) => {
+		const condition = e.getAttribute('d-if');
+
+		const siblingConditionalElms: Array<Element> = [];
+		let currentElm = e;
+		// recursively check for subsequent elements with the d-else of d-else-if attribute
+		while (currentElm.nextElementSibling) {
+			const nextElm = currentElm.nextElementSibling;
+			if (nextElm.getAttribute('d-else-if') !== null) {
+				siblingConditionalElms.push(nextElm);
+			} else if (nextElm.getAttribute('d-else') !== null) {
+				siblingConditionalElms.push(nextElm);
+				break;
+			}
+			currentElm = nextElm;
+		}
+
+		if (siblingConditionalElms == undefined) return;
+
+		const resetHTML = () => {
+			e.innerHTML = '<!-- d-if -->';
+			siblingConditionalElms.forEach((elm) => {
+				elm.innerHTML = '<!-- d-if -->';
+			});
+		};
+
+		let ifStatement = `if (${condition}) {
+            e.innerHTML = "${e.innerHTML}"
+        } `;
+
+		siblingConditionalElms.forEach((element, i) => {
+			const siblingHTML = element.innerHTML;
+			element.innerHTML = '<!-- d-if -->';
+			let statementDirective = 'else';
+			element.removeAttribute('d-else');
+			if (element.hasAttribute('d-else-if')) statementDirective = 'else if';
+			const condition = element.getAttribute('d-' + statementDirective.split(' ').join('-'));
+
+			if (statementDirective == 'else if') {
+				statementDirective = `else if (${condition})`;
+				element.removeAttribute('d-else-if');
+			}
+
+			ifStatement = `${ifStatement} ${statementDirective} {
+                siblingConditionalElms[${i}].innerHTML = "${siblingHTML}"
+            }`;
+		});
+
+		e.removeAttribute('d-if');
+		if (!condition) return;
+		resetHTML();
+		eval(ifStatement);
+
+		if (condition.includes('appState.contents.')) {
+			let reactiveProp: Array<string> | string | null | undefined = /appState\.contents\.[a-zA-Z]+/.exec(condition);
+			if (!reactiveProp || !reactiveProp[0]) return;
+			reactiveProp = reactiveProp[0].split('.')[2];
+			if (!reactiveProp) return;
+			appState.listen(reactiveProp, () => {
+				resetHTML();
+				eval(ifStatement);
+			});
+		}
+	});
+}
+
+export function hydrateElement(querySelector: string, appState: Reactive, eventListenerName: string, getCookie: CallableFunction, setCookie: CallableFunction, removeAttribute?: boolean) {
+	const queryName: Array<string> | null = /(?<=\[).+?(?=\])/.exec(querySelector);
+	if (!queryName || !queryName[0]) return;
+
+	const querySelectorAll = querySelector.split(':').join('\\3A ');
+	querySelector = queryName[0];
+	const elms = Array.from(document.querySelectorAll(querySelectorAll));
+	if (elms.length === 0) return;
+
+	elms.forEach(async (e: Element) => {
+		const hydrationFunction = e.getAttribute(`${querySelector}`);
+
+		if (removeAttribute === undefined || removeAttribute === true) {
+			e.removeAttribute(`${querySelector}`);
+		}
+
+		if (!hydrationFunction) return;
+
+		e.addEventListener(eventListenerName, () => {
+			eval(hydrationFunction);
+		});
+	});
+}
+
+export function hydrateModelAttributes(appState: Reactive) {
+	const modelElms = Array.from(document.querySelectorAll('input[d-model], textarea[d-model]'));
+	if (modelElms.length === 0) return;
+	modelElms.forEach((e: Element) => {
+		const modelName = e.getAttribute('d-model');
+		if (!modelName) return;
+		e.addEventListener('input', (input: Event) => {
+			const target = input.target as HTMLInputElement;
+			appState.contents[modelName] = target.value;
+		});
+	});
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function hydrateHeadElements(appState: Reactive) {
+	const headContent = document.head.innerHTML;
+	const headElements = Array.from(document.querySelectorAll('devto\\3A head'));
+	if (headElements.length === 0) return;
+	headElements.forEach((e: Element) => {
+		if (e.childNodes.length == 0) return;
+		e.childNodes.forEach((elm) => {
+			const child = elm as Element;
+			if (child.nodeType !== 1) return;
+			if (!document.head.querySelectorAll(child.tagName)) return;
+			document.head.appendChild(child);
+		});
+		e.remove();
+	});
+
+	document.addEventListener('router:naviagte', () => {
+		document.head.innerHTML = headContent;
+	}, { once: true });
+}
+
+export async function hydrateAnchorElements(reduceJavascript: boolean) {
+	const anchorElms = document.querySelectorAll('a');
+
+	Array.from(anchorElms).forEach((e: HTMLAnchorElement) => {
+		if (!reduceJavascript && e.href === window.location.href) {
+			e.setAttribute('link:active', '');
+			e.setAttribute('tabindex', '-1');
+		}
+		e.addEventListener('click', async (click: MouseEvent) => {
+			if (!event || click.ctrlKey) return;
+			const target = click.target as HTMLElement;
+			event.preventDefault();
+
+			if (!target) return;
+			const url: string | null = target.getAttribute('href');
+
+			if (!url) return;
+			await renderPage(url);
+		});
+	});
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function hydrateKeyDown(appState: Reactive) {
+	Array.from(document.body.querySelectorAll('*')).forEach((e: Element) => {
+		const keydownElms = Array.from(e.attributes).filter((arrElm: Attr) => {
+			return arrElm.name.startsWith('d-on:keydown');
+		});
+
+		if (keydownElms.length === 0) return;
+		keydownElms.forEach((attr: Attr) => {
+			const item = attr.name;
+			let key = item.split('.')[1]?.toLowerCase();
+			if (key && key?.length > 0) {
+				key = key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
+			}
+			e.addEventListener('keydown', (keydown: KeyboardEvent) => {
+				let keyName = keydown.key;
+				if (keyName === ' ') {
+					keyName = 'Space';
+				}
+				const firstLetter = keyName.split('')[0];
+				keyName = firstLetter?.toUpperCase() + keyName.slice(1).toLowerCase();
+				if (keyName === key) {
+					const itemCode = e.getAttribute(item);
+					if (!itemCode) return;
+					// Instead of using the `eval` function, we can use a `try...catch` block to
+					// execute the code and catch any potential errors.
+					try {
+						// We can use the `Function` constructor to create a new function from the
+						// code string, and then call that function to execute the code.
+						const fn = new Function(itemCode);
+						fn();
+					} catch (error) {
+						// If there is an error, we can log it to the console for debugging purposes.
+						console.error(error);
+					}
+				}
+			});
+		});
+	});
+}
---
+++ day74/src/lib/templateRenderer.ts
@@ -0,0 +1,243 @@
+import { debugMode } from '../main';
+import { LRUCache } from './lruCache';
+const templateCache = new LRUCache(15);
+
+function stringToHash(string: string) {
+	let hash = 0;
+
+	if (string.length == 0) return hash;
+
+	for (let i = 0; i < string.length; i++) {
+		const char = string.charCodeAt(i);
+		hash = ((hash << 5) - hash) + char;
+		hash = hash & hash;
+	}
+
+	return hash;
+}
+
+export const compileToString = async (template: string) => {
+	const templateHash = stringToHash(template).toString();
+
+	const cachedTemplate = templateCache.get(templateHash);
+	if (cachedTemplate) {
+		const {fnStr, styles, script, setupScript, head, layouts } = cachedTemplate;
+
+		if (debugMode) {
+			console.groupCollapsed('🗃️ loaded template from cache');
+			console.info('Template String: ' + fnStr);
+			console.groupEnd();
+		}
+
+		return { fnStr, styles, script, setupScript, head, layouts };
+	}
+
+	let styles = '';
+
+	const style = parseFromRegex(template, /<style[\s\S]*?>[\s\S]*?<\/style>/gi);
+
+	if (style) {
+		style.forEach(async (styleData) => {
+			if (!styleData) return;
+			if (styleData.startsWith('<style') && styleData.endsWith('</style>')) {
+				styles += styleData.split('<style')[1]?.split('>')[1]?.split('</style')[0];
+			}
+
+			// remove the style from the body
+			template = template.split('<style>' + styles + '</style>').join('');
+		});
+	}
+
+	let scriptInjection = '';
+	let script = '';
+	const meta = { layout: 'default', reduceJavascript: false, suspendUntilHydrated: true };
+
+	const scriptContent = parseFromRegex(template, /<script>[\s\S]*?<\/script>/gi);
+
+	if (scriptContent) {
+		scriptContent.forEach(async (scriptData) => {
+			if (!scriptData) return;
+			if (scriptData.startsWith('<script>') && scriptData.endsWith('</script>')) {
+				// if (scriptData.includes('appState.contents.')) {
+				// 	scriptInjection += 'const { getAppState, initAppState } = await import("/src/main.ts");\nasync initAppState();\nconst appState = getAppState();';
+				// }
+				const metaElms = parseFromRegex(scriptData, /definePageMeta\({(.*?)}\)(;){0,1}/g);
+				metaElms.forEach((metaElm: string | undefined) => {
+					if (!metaElm || !scriptData) return;
+					if (metaElm.startsWith('definePageMeta({')) {
+						scriptData = scriptData.split(metaElm).join('');
+						template = template.split(metaElm).join('');
+						let metaObjString = metaElm.split('(')[1]?.split(')')[0];
+						if (!metaObjString) return;
+						metaObjString = metaObjString.replaceAll(' ', '').replaceAll('{', '{\'').replaceAll(':', '\':').replaceAll(',', ',\'').replaceAll('\'', '"');
+						const newMeta = JSON.parse(metaObjString);
+						Object.keys(newMeta).forEach((key) => {
+							newMeta[key] = meta[key];
+						});
+					}
+				});
+
+				if (scriptData.includes('getCookie')) {
+					scriptInjection += 'const { getCookie } = await import("/src/lib/cookieManager.ts");';
+				}
+
+				if (scriptData.includes('setCookie')) {
+					scriptInjection += 'const { setCookie } = await import("/src/lib/cookieManager.ts");';
+				}
+
+				if (scriptData.includes('isSSR()')) {
+					scriptInjection += 'const { isSSR } = await import("/src/main.ts");';
+				}
+
+				script += scriptData.split('<script>')[1]?.split('</script>')[0];
+				const minishScript = script.replace(/[\n\r]/g, '').trim();
+				if (!minishScript) return;
+				template = template.split('<script>' + script + '</script>').join('');
+				scriptInjection += 'document.addEventListener(\'router:client:load\', () => {\n' + minishScript + '\n}, { once: true });';
+			}
+
+			// remove the script from the body
+			template = template.split('<script>' + script + '</script>').join('');
+			script = '';
+		});
+	}
+
+	const scriptContentSetup = parseFromRegex(template, /<script setup[\s\S]*?>[\s\S]*?<\/script>/gi);
+	let setupScriptInjection = '';
+	let setupScript = '';
+	if (scriptContentSetup) {
+		scriptContentSetup.forEach(async (scriptData) => {
+			if (!scriptData) return;
+			if (scriptData.startsWith('<script setup') && scriptData.endsWith('</script>')) {
+				// if (scriptData.includes('appState.contents.') && !scriptInjection.includes('initAppState')) {
+				// 	scriptInjection = 'const { getAppState, initAppState } = await import("/src/main.ts"); //aaahahahahahahah' + scriptInjection;
+				// }
+
+				if (scriptData.includes('getCookie')) {
+					setupScriptInjection += 'const { getCookie } = await import("/src/lib/cookieManager.ts");';
+				}
+
+				if (scriptData.includes('setCookie')) {
+					setupScriptInjection += 'const { setCookie } = await import("/src/lib/cookieManager.ts");';
+				}
+
+				if (scriptData.includes('isSSR()')) {
+					setupScriptInjection += 'const { isSSR } = await import("/src/main.ts");';
+				}
+
+				setupScript += scriptData.split('<script setup>')[1]?.split('</script>')[0];
+				setupScriptInjection = scriptData.split('<script setup>')[1]?.split('</script>')[0] + setupScriptInjection;
+			}
+
+			// remove the script from the body
+			template = template.split('<script setup>' + setupScript + '</script>').join('');
+		});
+	}
+
+	let headInjection = '';
+	let head = '';
+	if (import.meta.env.SSR) {
+		const headInjectionContent = parseFromRegex(template, /<devto:head[\s\S]*?>[\s\S]*?<\/devto:head>/gi);
+		if (headInjectionContent) {
+			headInjectionContent.forEach(async (headData) => {
+				if (!headData) return;
+				if (headData.startsWith('<devto:head>') && headData.endsWith('</devto:head>')) {
+					const newHeadContent = headData.split('<devto:head>')[1]?.split('</devto:head>')[0];
+					if (!newHeadContent) return;
+					head = newHeadContent;
+					const elements = newHeadContent.split('\n');
+					elements.forEach((e, i, arr) => {
+						if (!e.trim()) return;
+						function isCompleteElement() {
+							return e.endsWith('>');
+						}
+						if (!isCompleteElement()) {
+							arr[i] = '';
+							arr[i + 1] = e + arr[i + 1];
+						}
+						if (!isCompleteElement()) return;
+						headInjection += e;
+					});
+				}
+			});
+			template = template.split('<devto:head>' + head + '</devto:head>').join('');
+		}
+	}
+
+
+	if (template.includes('getCookie') && !script.includes('getCookie')) {
+		scriptInjection += 'const getCookie = await import("/src/lib/cookieManager.ts");';
+	}
+
+	if (template.includes('setCookie') && !script.includes('setCookie')) {
+		scriptInjection += 'const { setCookie } = await import("/src/lib/cookieManager.ts");';
+	}
+
+	const ast: (string | undefined)[] = parseFromRegex(template, /{(.*?)}/g);
+	let fnStr = '``';
+
+	if (!ast) return;
+
+	ast.forEach(async (t: string | undefined) => {
+		if (!t) return;
+		// checking to see if it is an interpolation
+		if (t.startsWith('{') && t.endsWith('}')) {
+			// TODO: rewrite comment
+			const bracketVariable = t.split(/{|}/).filter(Boolean)[0]?.trim();
+			if (!bracketVariable) return;
+			const parentElement = fnStr.split(t)[0]?.split('>');
+			if (!parentElement || !parentElement[parentElement.length - 2] || typeof parentElement[parentElement.length - 2] == 'undefined') return;
+			const isRawHTML = parentElement[parentElement.length - 2]?.includes('d-html');
+			if (bracketVariable.startsWith('appState.contents.')) {
+				const uuid = bracketVariable.substring(bracketVariable.length, 18).split('').map((c: string) => c.charCodeAt(0).toString(16).padStart(2, '0')).join('');
+				fnStr = fnStr.substring(0, fnStr.length - 1) + `<span data-token-${uuid}>\``;
+			} else {
+				fnStr = fnStr.substring(0, fnStr.length - 1) + '<span>`';
+			}
+			let runVar = `((${bracketVariable}).toString().replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;'))`;
+
+			if (isRawHTML) {
+				runVar = `(${bracketVariable})`;
+			}
+
+			fnStr += `+ (${runVar})` + '+`</span>`';
+		} else {
+			// append the string to the fnStr
+			fnStr += `+\`${t}\``;
+		}
+	});
+
+	if (debugMode) {
+		console.groupCollapsed('⚒️ Compiled template to String');
+		console.info('Template String: ' + fnStr);
+		console.groupEnd();
+	}
+
+
+	templateCache.set(templateHash, { fnStr, styles, script: scriptInjection, setupScript: setupScriptInjection, head: headInjection, layouts: meta });
+	return { fnStr, styles, script: scriptInjection, setupScript: setupScriptInjection, head: headInjection, layouts: meta };
+};
+
+function parseFromRegex(template: string, regex: RegExp) {
+	const matches = template.match(regex);
+	if (!matches) {
+		return [template];
+	}
+
+	const arr = [];
+	let startIndex = 0;
+	for (const match of matches) {
+		const matchIndex = template.indexOf(match, startIndex);
+		if (matchIndex > 0) {
+			arr.push(template.substring(startIndex, matchIndex));
+		}
+		arr.push(match);
+		startIndex = matchIndex + match.length;
+	}
+
+	if (startIndex < template.length) {
+		arr.push(template.substring(startIndex));
+	}
+
+	return arr;
+}
